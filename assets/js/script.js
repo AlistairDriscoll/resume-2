4a0cabce667f5ed1d2ee93c4bffaa7a0d9d49b2a
AlistairDriscoll <pallythecoder@gmail.com>
2024-05-08 18:40:50 +0100

    Change centre of map

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,12 @@ async function initMap() {
       "marker",
     );
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 3,
-      center: { lat: -28.024, lng: 140.887 },
-      mapId: "DEMO_MAP_ID",
+        zoom: 3,
+        center: {
+        lat: 46.619261,
+        lng: -33.134766
+    },
+        mapId: "DEMO_MAP_ID",
     });
     const infoWindow = new google.maps.InfoWindow({
       content: "",
