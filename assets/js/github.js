11a6cd91eec6994c224552063c006c503c73aa48
AlistairDriscoll <pallythecoder@gmail.com>
2024-05-13 15:31:36 +0100

    Add github page css and fix bugs

--- assets/js/github.js
+++ assets/js/github.js
@@ -37,6 +37,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
+
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -54,14 +57,17 @@ function fetchGitHubInformation(event) {
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
         function(firstResponse, secondResponse) {
-            var userData = firstResponse;
-            var repoData = secondResponse;
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
         }, function(errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found on this user ${username}</h2>`)
+            } else if(errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader("X-RateLimit-Reset") * 1000)
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
@@ -71,3 +77,5 @@ function fetchGitHubInformation(event) {
         }
     )
 }
+
+$(document).ready(fetchGitHubInformation);
